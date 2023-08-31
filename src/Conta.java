91f6e763d2b2841b8c57f937ef317b3dbc2ae5ec
Lucianogx <gxluciano@gmail.com>
2023-08-31 16:53:33 -0300

    mudança de nome da classe

--- src/Conta.java
+++ src/Conta.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App {
+public class Conta {
 
     public static void main(String[] args) throws Exception {
         DadosClientes cliente1 = new DadosClientes();
